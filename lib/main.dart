eba8193252fca1be429ef5c5c8ae6b5e16f5567d
kmh339 <kmh339@gmail.com>
2019-12-19 00:20:22 +0900

    firebase db start

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
